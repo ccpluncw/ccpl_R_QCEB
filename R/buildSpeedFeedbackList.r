be3be343042070a5ba849707f76f8c06d20ff97a
DJC <dalejcohen@gmail.com>
2022-02-08 14:41:04 -0500

    added some new functions

--- R/buildSpeedFeedbackList.r
+++ R/buildSpeedFeedbackList.r
@@ -4,14 +4,15 @@
 #' @param showFeedback  A boolean that specifies whether to show the speedFeedback. This is not a trial-by-trial variable.  It applies for the entire experiment.  So, set it to true if you want to give speedFeedback.  Set it to false if you do not want to give speedFeedback. Default = TRUE.
 #' @param speedThresholdUp  A boolean that specifies whether the speedFeedback is intended to speed the participant's responses up (make them faster) or slow the particpant down (make them slower).  Default = TRUE (make the participant faster).
 #' @param frameName A string specifying the name of the frame that is want to give speed feedback on.  This frameName should be unique and different from the frames that you do not want to provide speed feedback on. DEFAULT = "test"
-#' @param windowSize An integer specifying the number of frames (i.e., trials) on which you want to base your speedFeedback threshold. The program will take the most recent "windowSize" frames to create the RTwindowArray. The RTwindowArray will be used, in part, to determine the RT threshold of the next trial. DEFAULT = 20.
-#' @param thresholdArrayPosition An integer specifying the array position in the RTwindowArray, sorted from fastest to slowest, to use as the new potential RT threshold. So, if the thresholdArrayPosition = 18 and windowSize = 20, then the 18th slowest RT would be the new potential RT threshold. DEFAULT = 17.
+#' @param thresholdWindowSize An integer specifying the number of frames (i.e., trials) on which you want to base your speedFeedback threshold. The program will take the most recent "thresholdWindowSize" frames to create the RTwindowArray. The RTwindowArray will be used, in part, to determine the RT threshold of the next trial. DEFAULT = 20.
+#' @param thresholdArrayPosition An integer specifying the array position in the RTwindowArray, sorted from fastest to slowest, to use as the new potential RT threshold. So, if the thresholdArrayPosition = 18 and thresholdWindowSize = 20, then the 18th slowest RT would be the new potential RT threshold. DEFAULT = 17.
 #' @param thresholdBoundary An integer specifying a hard boundary that the RT threshold cannot cross.  If speedThresholdUp = TRUE, then the RT threshold cannot go below this boundary.  If speedThresholdUp = FALSE, then the RT threshold cannot go above this boundary. DEFAULT = NUll.  If NULL, then: thresholdBoundary = 250 if speedThresholdUp = TRUE and thresholdBoundary = 40000 if speedThresholdUp = FALSE.
-#' @param initialWindowRtThreshold An integer specifying the initial value of the RT threshold while the first "windowSize" trials are being accumulated.  DEFAULT = NULL. If NULL then: if speedThresholdUp = TRUE, then the initialWindowRtThreshold  = 999999 (very high so it is not activated).  If speedThresholdUp = FALSE, then  initialWindowRtThreshold = -1 (very low so it is not activated).
-#' @param initialWindowMessageDisplay A string that specifies the message to be shown to the participant after every trial during the initial period while the first "windowSize" trials are being accumulated. The string must be in html format.  You can use any html codes.  DEFAULT = "&nbsp" (blank).
-#' @param initialWindowMessageOutput A string that will be output in the datafile every trial during the initial period while the first "windowSize" trials are being accumulated. DEFAULT = "initial".
-#' @param initialStimulusDuration An integer specifying the time, in ms, that the feedback message should be presented during the initial period while the first "windowSize" trials are being accumulated. DEFAULT = 1 (not noticable)
-#' @param initialPostTrialGap An integer specifying the time, in ms, between the feedback message and the next frame during the initial period while the first "windowSize" trials are being accumulated. DEFAULT = 1 (not noticable)
+#' @param currentRTWindowSize An integer specifying the number of frames (i.e., trials) on which you want to base your current trial RT. The program will take the average RT of the most recent "currentRTWindowSize" frames to create the current trial RT. The current trial RT will be compared to the RT threshold. If currentRTWindowSize = 1, then the current trial alone is compared to the RT threshold.  If it is greater than 1, then the moving average of the currentRTWindowSize is compared to the RT threshold. currentRTWindowSize must be less than or equal to thresholdWindowSize. DEFAULT = 1 (the current trial alone).
+#' @param initialWindowRtThreshold An integer specifying the initial value of the RT threshold while the first "thresholdWindowSize" trials are being accumulated.  DEFAULT = NULL. If NULL then: if speedThresholdUp = TRUE, then the initialWindowRtThreshold  = 999999 (very high so it is not activated).  If speedThresholdUp = FALSE, then  initialWindowRtThreshold = -1 (very low so it is not activated).
+#' @param initialWindowMessageDisplay A string that specifies the message to be shown to the participant after every trial during the initial period while the first "thresholdWindowSize" trials are being accumulated. The string must be in html format.  You can use any html codes.  DEFAULT = "&nbsp" (blank).
+#' @param initialWindowMessageOutput A string that will be output in the datafile every trial during the initial period while the first "thresholdWindowSize" trials are being accumulated. DEFAULT = "initial".
+#' @param initialStimulusDuration An integer specifying the time, in ms, that the feedback message should be presented during the initial period while the first "thresholdWindowSize" trials are being accumulated. DEFAULT = 1 (not noticable)
+#' @param initialPostTrialGap An integer specifying the time, in ms, between the feedback message and the next frame during the initial period while the first "thresholdWindowSize" trials are being accumulated. DEFAULT = 1 (not noticable)
 #' @param aboveThresholdMessageDisplay A string that specifies the message to be shown to the participant when the current trial's RT is above the RT threshold. The string must be in html format.  You can use any html codes.  DEFAULT = NUll.  If NULL, then the message will be "Too Slow" if speedThresholdUp = TRUE and "&nbsp" if speedThresholdUp = FALSE.
 #' @param aboveThresholdMessageOutput A string that will be output in the datafile when the current trial's RT is above the RT threshold. DEFAULT = "above".
 #' @param aboveStimulusDuration An integer specifying the time, in ms, that the feedback message should be presented when the current trial's RT is above the RT threshold. DEFAULT = NUll.  If NULL, then aboveStimulusDuration = 500 if speedThresholdUp = TRUE and aboveStimulusDuration = 1 if speedThresholdUp = FALSE.
@@ -26,7 +27,7 @@
 #' @export
 #' @examples buildQCEdbFile (expName = "myExp", condName="TestCond", keyMap = myQCEBkeymap, randomizeKeyMap = TRUE, addQualtricsCode = TRUE, defaultBackgroundColor = "#000000", restAfterEveryNTrials = c(50, 100), instructionFile = "instructions.html", keyMapInstructionFile = "kmInst.html", getUserNameFile = NULL, getConsentFile = "consent.html", getDemographicsFile = NULL, getGenderFile = NULL, welcomeMsg = NULL, restMsg = NULL, endOfExpMsg = NULL, saveMsg = NULL)
 
-buildSpeedFeedbackList <- function (showFeedback = TRUE, speedThresholdUp = TRUE, frameName = "test", windowSize = 20, thresholdArrayPosition = 17, thresholdBoundary = NULL, initialWindowRtThreshold = NULL, initialWindowMessageDisplay = "&nbsp", initialWindowMessageOutput = "initial", initialStimulusDuration = 1, initialPostTrialGap = 1, aboveThresholdMessageDisplay = NULL, aboveThresholdMessageOutput = "above", aboveStimulusDuration = NULL, abovePostTrialGap= NULL, belowThresholdMessageDisplay = NULL, belowThresholdMessageOutput = "below", belowStimulusDuration = NULL, belowPostTrialGap = NULL) {
+buildSpeedFeedbackList <- function (showFeedback = TRUE, speedThresholdUp = TRUE, frameName = "test", thresholdWindowSize = 20, thresholdArrayPosition = 17, thresholdBoundary = NULL, currentRTWindowSize = 1, initialWindowRtThreshold = NULL, initialWindowMessageDisplay = "&nbsp", initialWindowMessageOutput = "initial", initialStimulusDuration = 1, initialPostTrialGap = 1, aboveThresholdMessageDisplay = NULL, aboveThresholdMessageOutput = "above", aboveStimulusDuration = NULL, abovePostTrialGap= NULL, belowThresholdMessageDisplay = NULL, belowThresholdMessageOutput = "below", belowStimulusDuration = NULL, belowPostTrialGap = NULL) {
 
   if(!is.logical(showFeedback)) {
     stop("showFeedback option must be a boolean (TRUE/FALSE).  Yours, apparently, is not.")
@@ -55,8 +56,16 @@ buildSpeedFeedbackList <- function (showFeedback = TRUE, speedThresholdUp = TRUE
     stop("belowThresholdMessageOutput option must be a single string.  Yours, apparently, is not a single string.")
   }
 
-  if(!isSingleNumeric(windowSize)) {
-    stop("windowSize option must be a single integer.  Yours, apparently, is not.")
+  if(!isSingleNumeric(thresholdWindowSize)) {
+    stop("thresholdWindowSize option must be a single integer.  Yours, apparently, is not.")
+  }
+
+  if(!isSingleNumeric(currentRTWindowSize)) {
+    stop("currentRTWindowSize option must be a single integer.  Yours, apparently, is not.")
+  }
+
+  if((currentRTWindowSize > thresholdWindowSize)) {
+    stop("currentRTWindowSize option must be less than or equal to thresholdWindowSize.  Yours, apparently, is not.")
   }
 
   if(!isSingleNumeric(thresholdArrayPosition)) {
@@ -204,7 +213,7 @@ buildSpeedFeedbackList <- function (showFeedback = TRUE, speedThresholdUp = TRUE
   }
 
 
-  tmpList <- list (showFeedback = showFeedback, speedThresholdUp = speedThresholdUp, frameName = frameName, windowSize = windowSize, thresholdArrayPosition = thresholdArrayPosition, thresholdBoundary = thresholdBoundary, initialWindowRtThreshold = initialWindowRtThreshold, initialWindowMessageDisplay = initialWindowMessageDisplay, initialWindowMessageOutput = initialWindowMessageOutput, initialStimulusDuration = initialStimulusDuration, initialPostTrialGap = initialPostTrialGap, aboveThresholdMessageDisplay = aboveThresholdMessageDisplay, aboveThresholdMessageOutput = aboveThresholdMessageOutput, aboveStimulusDuration = aboveStimulusDuration, abovePostTrialGap= abovePostTrialGap, belowThresholdMessageDisplay = belowThresholdMessageDisplay, belowThresholdMessageOutput = belowThresholdMessageOutput, belowStimulusDuration = belowStimulusDuration, belowPostTrialGap = belowPostTrialGap)
+  tmpList <- list (showFeedback = showFeedback, speedThresholdUp = speedThresholdUp, frameName = frameName, thresholdWindowSize = thresholdWindowSize, thresholdArrayPosition = thresholdArrayPosition, currentRTWindowSize = currentRTWindowSize, thresholdBoundary = thresholdBoundary, initialWindowRtThreshold = initialWindowRtThreshold, initialWindowMessageDisplay = initialWindowMessageDisplay, initialWindowMessageOutput = initialWindowMessageOutput, initialStimulusDuration = initialStimulusDuration, initialPostTrialGap = initialPostTrialGap, aboveThresholdMessageDisplay = aboveThresholdMessageDisplay, aboveThresholdMessageOutput = aboveThresholdMessageOutput, aboveStimulusDuration = aboveStimulusDuration, abovePostTrialGap= abovePostTrialGap, belowThresholdMessageDisplay = belowThresholdMessageDisplay, belowThresholdMessageOutput = belowThresholdMessageOutput, belowStimulusDuration = belowStimulusDuration, belowPostTrialGap = belowPostTrialGap)
 
   return(tmpList)
 
